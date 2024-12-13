52efb6eef4c7043bd0d79255f3c3888cdd78ebcb
Danny Wu <dannywu826@gmail.com>
2024-12-13 06:43:02 -0500

    Puppeteer returning Amazon

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ import {
   writeResponseToNodeResponse,
 } from '@angular/ssr/node';
 import express from 'express';
+import puppeteer from 'puppeteer';
+import bodyParser from 'body-parser';
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
@@ -14,18 +16,7 @@ const browserDistFolder = resolve(serverDistFolder, '../browser');
 const app = express();
 const angularApp = new AngularNodeAppEngine();
 
-/**
- * Example Express Rest API endpoints can be defined here.
- * Uncomment and define endpoints as necessary.
- *
- * Example:
- * ```ts
- * app.get('/api/**', (req, res) => {
- *   // Handle API request
- * });
- * ```
- */
-
+app.use(bodyParser.json());
 /**
  * Serve static files from /browser
  */
@@ -36,6 +27,58 @@ app.use(
     redirect: false,
   }),
 );
+
+app.post('/scrape', async (req, res) => {
+
+  const { searchValue } = req.body;
+  if (!searchValue) {
+    return res.status(400).json({ error: 'Missing search value' });
+  }
+
+  try {
+    const browser = await puppeteer.launch();
+
+    const page = await browser.newPage();
+
+    const amazonUrl = `https://www.amazon.com/s?k=${searchValue}`;
+    await page.goto(amazonUrl, { waitUntil: 'domcontentloaded' });
+
+    page.on('console', async (msg) => {
+      const msgArgs = msg.args();
+      for (let i = 0; i < msgArgs.length; ++i) {
+        console.log(await msgArgs[i].jsonValue());
+      }
+    });
+    const amazonProducts = await page.evaluate(() => {
+      const titleElements = Array.from(document.querySelectorAll('div[data-cy="title-recipe"] h2 span'));
+      const imageElements = Array.from(document.querySelectorAll('div.a-section.aok-relative.s-image-square-aspect img'));
+      const priceElements = Array.from(document.querySelectorAll('.a-price .a-offscreen'));
+      const buyLink = document.querySelector('a.a-link-normal.s-line-clamp-4.s-link-style.a-text-normal')?.getAttribute('href');
+
+      return titleElements.map((item, index) => {
+        const title = item?.textContent?.trim() || 'No title available';
+        const imageUrl = imageElements[index]?.getAttribute('src') || 'No image available';
+        const price = priceElements[index]?.textContent?.trim() || 'No price available';
+        const fullUrl = buyLink ? `https://www.amazon.com${buyLink}` : null;
+
+        if (!title.includes("Roland")) {
+          return null;
+        }
+
+        return { title, imageUrl, price, fullUrl };
+      });
+    });
+
+    return res.json({
+      results: amazonProducts
+    })
+
+  } catch (error) {
+    console.error('Error during web scraping:', error);
+    return res.status(500).json({ error: 'Web scraping failed' });
+  }
+})
+
 
 /**
  * Handle all other requests by rendering the Angular application.
